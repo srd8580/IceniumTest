a8f59d38dbe3d8447d76879e65ba77e83b50cd9c
Steve Danner <steve.danner@knowledgelake.com>
2013-07-23 07:33:01 -0500

    Changes made by Steve Danner (steve.danner@knowledgelake.com).

--- Search Mobile Prototype/scripts/App.js
+++ Search Mobile Prototype/scripts/App.js
@@ -9,27 +9,54 @@ require.config({
     baseUrl: 'scripts',
     paths: {
         jquery: 'lib/jquery',
+        childBrowser: 'lib/childBrowser',
         knockout: 'lib/knockout',
         domReady: 'lib/domReady',
         kendo: 'lib/kendo.mobile.min',
-        partialView: 'knockout/partialView'
-    },
+        partialView: 'knockout/partialView',
+        WL: 'lib/WL',
+        SPServices: 'lib/jquery.SPServices',
+        md5: 'lib/md5',
+        jqueryClass: 'lib/jquery.class',
+        digestAuth: 'lib/digest-auth'
+    },    
     shim: {
         jquery: {
             exports: 'jquery'
         },
+        md5: {
+            exports: 'md5',
+            deps: ['jquery']
+        },
+        jqueryClass: {
+            exports: 'jqueryClass',
+            deps: ['jquery']            
+        },
+        digestAuth: {
+            exports: 'digestAuth',
+            deps: ['jquery', 'md5', 'jqueryClass']
+        },
+        childBrowser: {
+            exports: 'childBrowser'  
+        },     
         knockout: {
             exports: 'ko'
         },
         kendo: {            
             deps: ['jquery'],
             exports: 'kendo'
+        },
+        WL: {
+            exports: 'WL'
+        },
+        SPServices: {
+            exports: 'SPServices'
         }
     }
 });
 
-require(["jquery", "knockout", "domReady", "kendo", "app/Utility", "app/AppViewModel", "partialView"],
-    function ($, ko, domReady, kendo, Utility, AppViewModel, partialView) {
+require(["jquery", "knockout", "domReady", "kendo", "md5", "jqueryClass", "digestAuth", "app/Utility", "app/AppViewModel", "partialView"],
+    function ($, ko, domReady, kendo, md5, jqueryClass, digestAuth, Utility, AppViewModel, partialView) {
         window.AppViewModel = new AppViewModel();
         ko.applyBindings(window.AppViewModel, document.body);                      
         
